29f7219afd3a207db1dd9839cc0bddb717a7d6de
Zulfiqor <theassassinaa@gmail.com>
2025-02-07 16:03:13 +0500

    Its not Recursion Project

--- 04.02.25 Student/Console/obj/Debug/net9.0/Console.AssemblyInfo.cs
+++ 04.02.25 Student/Console/obj/Debug/net9.0/Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f8987a3115c8846ae75aa8adc27723b0d05fc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9bea76d6b2205fe7e880ef8bf0ecffce5e21baa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
